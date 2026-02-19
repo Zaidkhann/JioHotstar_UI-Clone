a76f46699cf5ffdc7d5f81cfd4dda0b9488d68cb
Zaid khan <mkzaid46@gmail.com>
2026-02-19 23:03:28 +0530

    d

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ buttonOTP.addEventListener("click",
         }
         else{
         errorhandle.style.display = "none";
-        window.location.href = "index.html";
+        window.location.href = "log.html";
         }
         
     })
